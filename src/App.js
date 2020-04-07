6be5822b0087c5af3a232cffbef3b34e2a5daa12
farzadjamshidi <farzadjamshidicom@yahoo.com>
2020-04-08 03:33:52 +0430

    add some sample pictures and show in left sidebar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React from 'react';
 // import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 import { makeStyles } from '@material-ui/core/styles';
-import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
 
 const useStyles = makeStyles((theme) => ({
@@ -22,12 +21,34 @@ function App() {
 
   const classes = useStyles();
 
+  const images = [
+    "00.jpg",
+    "01.jpg",
+    "02.jpg",
+    "03.jpg",
+    "04.jpg",
+    "05.jpg",
+    "06.jpg",
+    "07.jpg",
+    "08.jpg",
+    "09.jpg",
+    "10.jpg",
+  ];
+
   return (
     <div className={classes.root}>
-      <Grid container spacing={3} direction='row'>
+      <Grid container spacing={1} direction='row'>
 
         <Grid item xs={2}>
-          <div className="full-height left-sidebar" />
+          <div className="full-height left-sidebar overflow">
+            {images.map((item) => (
+
+              <div className="left-sidebar-item">
+                  <img src={item} alt="" className="left-sidebar-item-container"/>
+              </div>
+            ))
+            }
+          </div>
         </Grid>
         <Grid item xs={8}>
           <div className="full-height">
